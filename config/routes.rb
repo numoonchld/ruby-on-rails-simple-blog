09c5ef3a34ecae69c858a312ed3b8062d2b86453
Raghavendra Saralaya <17598505+numoonchld@users.noreply.github.com>
2021-09-07 17:42:01 +0530

    added resources controllers

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     root 'articles#index'
 
-    get '/articles', to: 'articles#index'
-    get '/articles/:id', to: 'articles#show'
+    # get '/articles', to: 'articles#index'
+    # get '/articles/:id', to: 'articles#show'
+    resources :articles
   end
